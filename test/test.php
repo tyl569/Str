626f7c97e4067584429cb5ae1b279d5c27805232
tyl569 <569914877@qq.com>
2020-11-23 23:10:35 +0800

    feat: 增加代码运行时间比较

--- test/test.php
+++ test/test.php
@@ -1,5 +1,35 @@
 <?php
 
-echo Str\Camel::convert("snake_name","_");
-echo Str\Camel::convert("snake-name","-");
-echo Str\Camel::convert("snake name"," ");
+$runTime = 1000000;
+
+$start = microtime(true) * 1000;
+for ($i = 1; $i <= $runTime; $i++) {
+    convert1("snake-name", "-");
+}
+$end = microtime(true) * 1000;
+echo "php代码运行时间:" . ($end - $start) . "\n";
+
+$start = microtime(true) * 1000;
+for ($i = 1; $i <= $runTime; $i++) {
+    Str\Camel::convert("snake-name", "-");
+}
+$end = microtime(true) * 1000;
+echo "扩展运行时间" . ($end - $start) . "\n";
+
+function convert1($str, $symbol)
+{
+    $pos = null;
+    $after = "";
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str{$i} == $symbol) {
+            $pos = $i + 1;
+            $i++;
+        }
+        if ($pos === $i) {
+            $after .= strtoupper($str{$i});
+        } else {
+            $after .= $str{$i};
+        }
+    }
+    return $after;
+}
